e46db3f33565781ce7142d25b9c871c0c4566674
minhaz <minhazav@gmail.com>
2016-10-19 23:36:45 +0530

    Added code to poll for response from bot
    For quick implementation, the extensions polls for response in 5s
    interval for max 30 times. Good enough for a POC

--- Extension/todofy-live-help-poc/src/extension.ts
+++ Extension/todofy-live-help-poc/src/extension.ts
@@ -9,11 +9,14 @@ var request = require('request');
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+
+    // Data interface
     interface dataToBot {
         username: string;
         message: string;
         filename: string;
         question: string;
+        config: any;
     };
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
@@ -24,34 +27,37 @@ export function activate(context: vscode.ExtensionContext) {
     let config = new Config();
     
     let disposable = vscode.commands.registerCommand('extension.todofyLive', () => {
-        // The code you place here will be executed every time your command is executed
+        var editor, data:dataToBot, selection, text:string, regex, result;
+
+        // configuration should be set
         if (!config.Load()) {
             vscode.window.showInformationMessage('Please configure todofylivehelp');
             return;            
         }
+        
+        // define empty data
+        data = {
+            message: "",
+            username: null,
+            filename: null,
+            question: null,
+            config: config.config
+        };
 
-        // Display a message box to the user
-        var editor = vscode.window.activeTextEditor;
+        editor = vscode.window.activeTextEditor;
         if (!editor) {
             return; // No open text editor
         }
 
-        var selection = editor.selection;
-        var text:string = editor.document.getText(selection);
-        var data:dataToBot = {
-            message: "",
-            username: null,
-            filename: null,
-            question: null  
-        };
 
+        selection = editor.selection;
+        text = editor.document.getText(selection).trim();
         if (!text.length) {
-            text = editor.document.lineAt(selection.active.line).text;
+            text = editor.document.lineAt(selection.active.line).text.trim();
         }
-
-        text = text.trim();
-        var regex = /(.*)\@help:([a-zA-Z0-9]*)\s*.*/;
-        var result = regex.exec(text);
+        
+        regex = /(.*)\@help:([a-zA-Z0-9]*)\s*.*/;
+        result = regex.exec(text);
         if (typeof result[1] != 'undefined' && typeof result[2] != 'undefined') {
             data.question = result[1].trim();
             data.username = result[2].trim();
@@ -61,18 +67,21 @@ export function activate(context: vscode.ExtensionContext) {
             vscode.window.showInformationMessage('Help request sent to: ' +data.username);
             todofyLive.statusBarMessage('Help request sent to: ' +data.username);
 
-            // TODO: get 3 lines from top, 3 lines from bottom and send it to slack
+            // get 3 lines from top, 3 lines from bottom and send it to slack
+            // TODO: make this configurable by adding something like @lines[-10,+10]
             var start = (selection.active.line - 3 > 0) ? selection.active.line - 3 : 0;
             var end = (selection.active.line + 3 < editor.document.lineCount) ? selection.active.line + 3 : editor.document.lineCount;
             for (var i = start; i <= end; i++) data.message += editor.document.lineAt(i).text +"\n";
 
-            console.log(data);
-
-
+            // send the request to slackbot
+            request.post(config.config.EndPoint, {form: data}, function(a, b, c) {
+                var response = JSON.parse(c);
+                var guid = response.guid;
+                new ResponsePoll().call(data.config, guid, 0);
+            });
         } else {
             todofyLive.hideStatusBar();
         }
-        
     });
     
     context.subscriptions.push(todofyLive);
@@ -86,39 +95,6 @@ export function deactivate() {
 class TodofyLive {
     private _statusBarItem: StatusBarItem;
     private _timeout: any = null;
-
-    // constructor
-    TodofyLive() {
-        this._statusBarItem = window.createStatusBarItem(StatusBarAlignment.Left);
-    }
-
-    // public function
-    public updateStatusBar() {
-        // Create as needed
-        if (!this._statusBarItem) {
-            this._statusBarItem = window.createStatusBarItem(StatusBarAlignment.Left);
-        }
-
-        // Get the current text editor
-        let editor = window.activeTextEditor;
-        if (!editor) {
-            this._statusBarItem.hide();
-            return;
-        }
-
-        let doc = editor.document;
-
-        let TODOCount = this._getTODOCount(doc);
-
-        // Update the status bar
-        this._statusBarItem.text = TODOCount !== 1 ? `${TODOCount} TODOs` : '1 TODO';
-        this._statusBarItem.show();
-    }
-
-    public _getTODOCount(doc: TextDocument): number {
-        let docContent = doc.getText().toLowerCase();
-        return docContent.split('todo').length;
-    }
 
     public statusBarMessage(message) {
         if (!this._statusBarItem) {
@@ -143,11 +119,11 @@ class TodofyLive {
 }
 
 class Config {
-    private config: any;
+    public config: any;
 
     public Load() {
         var config = vscode.workspace.getConfiguration('Todofylive.SlackBot');
-        var requiredProps = ['APIToken', 'EndPoint', 'Channel', 'Team'];
+        var requiredProps = ['APIToken', 'EndPoint', 'Channel', 'Team', 'EndPointPoll'];
         // Verify if all config exist
         for (var i = 0 ; i < requiredProps.length; i++) {
             if (typeof config[requiredProps[i]] == 'undefined' || config[requiredProps[i]] == null) return false;
@@ -156,7 +132,27 @@ class Config {
         this.config = config;
         return true;
     }
+
     dispose() {
         this.config.dispose();
+    }
+}
+
+class ResponsePoll {
+    // TODO: make this configurable
+    private attemptThreshold = 30;
+    public call(config, guid, attempt = 0) {
+        if (attempt > this.attemptThreshold) return;
+        var that = this;
+        request.post(config.EndPointPoll, {form: {guid: guid}}, function(a, b, c) {
+            var response = JSON.parse(c);
+            if (response.ack) {
+                vscode.window.showInformationMessage('HELP RESPONSE: ' +response.text);       
+            } else {
+                setTimeout(function() {
+                    that.call(config, guid, attempt + 1);
+                }, 5000);
+            }
+        })
     }
 }
